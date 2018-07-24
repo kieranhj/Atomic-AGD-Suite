b286915332ddc2b8f53b2d77a4b1c15e6ed7da8e
kieranhj <kieran@kconnell.co.uk>
2018-07-24 22:43:03 +0100

    Relocate game code down to &E00 after load
    Load at &1100 which is safe for DFS but issue *TAPE and relocate over
    DFS workspace after load

--- BBC Suite/CC65/game.asm
+++ BBC Suite/CC65/game.asm
@@ -3,7 +3,8 @@
 ; Z80 conversion by Kees van Oss 2017
 ; BBC Micro version by Kieran Connell 2018
 ;----------------------------------------------
-	.DEFINE asm_code $1100	; assembly address _BEEB
+	.DEFINE asm_code $0e00	; assembly address _BEEB
+	.DEFINE load_address $1100	; load address _BEEB
 	.DEFINE header   0		; Header Wouter Ras emulator
 	.DEFINE filenaam "AGD"
 
@@ -27,9 +28,37 @@ name_end:
 ;********************************************************************
 .ENDIF
 
-exec:
 start_asm:
 	.include "z80-zp.inc"
+
+.segment "CODE"
+
+	jmp relocate + load_address - asm_code
+
+exec_game:
 	.include "game.inc"
 	.include "z80.asm"
+
 eind_asm:
+
+relocate:
+; Issue *TAPE otherwise DFS goes mental that we've overwritten workspace from &E00 - &1100
+
+    lda #$8C
+    ldx #$0C
+    ldy #$00
+    jsr OSBYTE					; *FX &8C,0,0 - *TAPE 1200
+
+; Relocate all code down to &E00
+	ldx #>(eind_asm - start_asm) + 1
+	ldy #0
+reloop:
+	lda load_address, y
+	sta asm_code, y
+	iny
+	bne reloop
+	inc reloop + 2 + load_address - asm_code
+	inc reloop + 5 + load_address - asm_code
+	dex
+	bne reloop
+	jmp exec_game
